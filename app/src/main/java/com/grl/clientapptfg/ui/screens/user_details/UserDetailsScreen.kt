104e029bb3407e778db207abc9f73d931665c842
gabri <gabrielrl2004@gmail.com>
2024-05-25 17:48:04 +0200

    Mejora de la pantalla user details

--- app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsScreen.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsScreen.kt
@@ -1,8 +1,6 @@
 package com.grl.clientapptfg.ui.screens.user_details
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,15 +10,23 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.grl.clientapptfg.core.UserSession
+import com.grl.clientapptfg.ui.components.ConfirmationDialog
 import com.grl.clientapptfg.ui.components.ConfirmationDialogWithNegative
+import com.grl.clientapptfg.ui.components.ProgressBarDialog
+import com.grl.clientapptfg.ui.components.StyledText
+import com.grl.clientapptfg.ui.components.TextAreaPersonalized
+import com.grl.clientapptfg.ui.components.TextFieldPersonalized
 import com.grl.clientapptfg.ui.screens.profile.ProfileViewModel
 import com.grl.clientapptfg.ui.screens.tabs_menu.TabsMenuViewModel
 import com.grl.clientapptfg.ui.screens.tracking.TrackingViewModel
@@ -41,45 +47,84 @@ fun UserDetailsScreen(
     trackingViewModel: TrackingViewModel
 ) {
     val aladinFont = Util.loadFontFamilyFromAssets()
-    val closeSession = userDetailsViewModel.closeSession.observeAsState(initial = false)
+    val user = UserSession.getUser()!!
+    val closeSession by userDetailsViewModel.closeSession.observeAsState(initial = false)
+    val address by userDetailsViewModel.address.observeAsState(user.address)
+    val phone by userDetailsViewModel.phone.observeAsState(initial = user.phone)
+    val isUpdateEnable by userDetailsViewModel.isUpdateEnable.observeAsState(initial = true)
+    val isEditing by userDetailsViewModel.isEditing.observeAsState(false)
+    val updateWasGood by userDetailsViewModel.updateWasGood.observeAsState(initial = false)
+    val isLoading by userDetailsViewModel.isLoading.observeAsState(false)
+    val wantToUpdate by userDetailsViewModel.wantToUpdate.observeAsState(false)
+
+    if (closeSession) {
+        ConfirmationDialogWithNegative(
+            onPositive = {
+                userDetailsViewModel.changeCloseSession(false)
+                UserSession.setUser(null)
+                tabsMenuViewModel.cleanList()
+                trackingViewModel.cleanOrderList()
+                tabsMenuViewModel.updateTotalPrice()
+                profileViewModel.setScreenState(1)
+            },
+            title = "¿Estas seguro de cerrar sesión?",
+            text = "Pulsa aceptar para salir",
+            onNegative = { userDetailsViewModel.changeCloseSession(false) })
+    }
+
+    if (wantToUpdate) {
+        ConfirmationDialogWithNegative(
+            onPositive = {
+                userDetailsViewModel.updateUser(address, phone, user)
+                userDetailsViewModel.changeWantToUpdate(false)
+            },
+            title = "¿Estas seguro de que quieres actualizar?",
+            text = "Pulsa aceptar para actualizar"
+        ) {
+            userDetailsViewModel.changeWantToUpdate(false)
+        }
+    }
+
+    if (isLoading) {
+        ProgressBarDialog()
+    }
+
+    if (updateWasGood) {
+        ConfirmationDialog(
+            onClick = {
+                userDetailsViewModel.changeIsEditing(false)
+                userDetailsViewModel.changeUpdateWasGood(false)
+            },
+            title = "La cuenta ha sido actualizada con éxito",
+            text = "Pulsa aceptar para continuar"
+        )
+    }
+
     ConstraintLayout(
         Modifier
             .fillMaxSize()
             .background(granate)
     ) {
-        val (title, body, divider, button) = createRefs()
+        val (title, divider, updateBtn, closeBtn, userName,
+            userAdd, userTelf, userEmail, userAddField, userTelfField) = createRefs()
         val topGuide = createGuidelineFromTop(0.03f)
-        val startGuide = createGuidelineFromStart(0.05f)
-        val endGuide = createGuidelineFromEnd(0.05f)
-
-        if (closeSession.value) {
-            ConfirmationDialogWithNegative(
-                onPositive = {
-                    userDetailsViewModel.changeCloseSession(false)
-                    UserSession.setUser(null)
-                    tabsMenuViewModel.cleanList()
-                    trackingViewModel.cleanOrderList()
-                    tabsMenuViewModel.updateTotalPrice()
-                    profileViewModel.setScreenState(1)
-                },
-                title = "¿Estas seguro de cerrar sesión?",
-                text = "Pulsa aceptar para salir",
-                onNegative = { userDetailsViewModel.changeCloseSession(false) })
-        }
+        val bottomGuide = createGuidelineFromBottom(0.02f)
+        val startGuide = createGuidelineFromStart(0.02f)
+        val endGuide = createGuidelineFromEnd(0.02f)
 
         Text(
             text = "Detalles de mi cuenta",
-            Modifier
+            fontWeight = FontWeight.Bold,
+            fontSize = 50.sp,
+            fontFamily = aladinFont,
+            color = white,
+            modifier = Modifier
                 .padding(bottom = 10.dp)
                 .constrainAs(title) {
                     top.linkTo(topGuide)
                     start.linkTo(startGuide)
                     end.linkTo(endGuide)
-                },
-            fontWeight = FontWeight.Bold,
-            fontSize = 50.sp,
-            fontFamily = aladinFont,
-            color = white
+                }
         )
         HorizontalDivider(
             Modifier
@@ -87,67 +132,158 @@ fun UserDetailsScreen(
                     top.linkTo(title.bottom)
                 }
                 .padding(horizontal = 20.dp)
-                .padding(bottom = 40.dp),
+                .padding(bottom = 30.dp),
             color = mostaza,
             thickness = 3.dp
         )
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(horizontal = 20.dp)
-                .constrainAs(body) {
+        StyledText(
+            fieldText = "- Nombre: ",
+            valueText = user.name,
+            modifier = Modifier
+                .padding(bottom = 25.dp)
+                .constrainAs(userName) {
                     top.linkTo(divider.bottom)
                     start.linkTo(startGuide)
+                })
+        StyledText(
+            fieldText = "- Correo: ",
+            valueText = user.gmail,
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .constrainAs(userEmail) {
+                    top.linkTo(userName.bottom)
+                    start.linkTo(startGuide)
+                })
+        Text(
+            text = "- Teléfono:",
+            color = mostaza,
+            fontFamily = aladinFont,
+            fontSize = 35.sp,
+            modifier = Modifier
+                .padding(end = 5.dp)
+                .constrainAs(userTelf) {
+                    top.linkTo(userEmail.bottom)
+                    start.linkTo(startGuide)
+                })
+        TextFieldPersonalized(
+            value = phone,
+            function = {
+                userDetailsViewModel.setPhone(it)
+                userDetailsViewModel.enableUpdate(address, phone)
+            },
+            modifier = Modifier
+                .height(60.dp)
+                .constrainAs(userTelfField) {
+                    top.linkTo(userTelf.top)
+                    bottom.linkTo(userTelf.bottom)
+                    start.linkTo(userTelf.end)
                     end.linkTo(endGuide)
-                }) {
-            Row {
-                DetailsText(text = "- Nombre: ", color = mostaza, modifier = Modifier.padding(bottom = 30.dp))
-                DetailsText(text = UserSession.getUser()!!.name, color = white, Modifier.padding(bottom = 30.dp))
-            }
-            Row {
-                DetailsText(text = "- Teléfono: ", color = mostaza, Modifier.padding(bottom = 30.dp))
-                DetailsText(text = UserSession.getUser()!!.phone, color = white, Modifier.padding(bottom = 30.dp))
-            }
-            DetailsText(text = "- Correo: ", color = mostaza)
-            DetailsText(text = UserSession.getUser()!!.gmail, color = white, Modifier.padding(bottom = 30.dp, start = 30.dp))
-            DetailsText(text = "- Dirección: ", color = mostaza)
-            DetailsText(text = UserSession.getUser()!!.address, color = white, Modifier.padding(bottom = 25.dp, start = 30.dp))
+                    width = Dimension.fillToConstraints
+                },
+            placeholder = "Debe tener 9 dígitos",
+            keyboardType = KeyboardType.Phone,
+            imeAction = ImeAction.Next,
+            enabled = isEditing
+        )
+        Text(
+            text = "- Dirección:",
+            color = mostaza,
+            fontFamily = aladinFont,
+            fontSize = 35.sp,
+            modifier = Modifier
+                .padding(top = 25.dp)
+                .constrainAs(userAdd) {
+                    top.linkTo(userTelfField.bottom)
+                    start.linkTo(startGuide)
+                })
+        TextAreaPersonalized(
+            value = address,
+            function = {
+                userDetailsViewModel.setAddress(it)
+                userDetailsViewModel.enableUpdate(address, phone)
+            },
+            modifier = Modifier
+                .padding(bottom = 15.dp)
+                .constrainAs(userAddField) {
+                    top.linkTo(userAdd.bottom)
+                    end.linkTo(endGuide)
+                    start.linkTo(startGuide)
+                    bottom.linkTo(closeBtn.top)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                },
+            placeholder = "Debe de tener al menos 5 caracteres",
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Default,
+            enabled = isEditing
+        )
+        Button(
+            onClick = {
+                if (!isEditing)
+                    userDetailsViewModel.changeIsEditing(true)
+                else
+                    userDetailsViewModel.changeWantToUpdate(true)
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 5.dp)
+                .height(75.dp)
+                .constrainAs(updateBtn) {
+                    bottom.linkTo(bottomGuide)
+                    start.linkTo(startGuide)
+                    end.linkTo(closeBtn.start)
+                    width = Dimension.fillToConstraints
+                },
+            colors = ButtonColors(
+                contentColor = black,
+                containerColor = mostaza,
+                disabledContainerColor = mostazaSoft,
+                disabledContentColor = blackSoft
+            ),
+            enabled = isUpdateEnable
+        ) {
+            Text(
+                text = if (!isEditing) "Modificar" else "Confirmar",
+                fontFamily = aladinFont,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
         Button(
             onClick = {
-                userDetailsViewModel.changeCloseSession(true)
+                if (!isEditing)
+                    userDetailsViewModel.changeCloseSession(true)
+                else {
+                    userDetailsViewModel.setPhone(user.phone)
+                    userDetailsViewModel.setAddress(user.address)
+                    userDetailsViewModel.enableUpdate(address, phone)
+                    userDetailsViewModel.changeIsEditing(false)
+                }
             },
             Modifier
                 .fillMaxWidth()
-                .height(115.dp)
-                .padding(horizontal = 20.dp, vertical = 20.dp)
-                .constrainAs(button) {
-                    bottom.linkTo(parent.bottom)
+                .height(75.dp)
+                .padding(start = 5.dp)
+                .constrainAs(closeBtn) {
+                    bottom.linkTo(bottomGuide)
+                    end.linkTo(endGuide)
+                    start.linkTo(updateBtn.end)
+                    width = Dimension.fillToConstraints
                 },
             colors = ButtonColors(
                 contentColor = black,
                 containerColor = darkRed,
                 disabledContainerColor = mostazaSoft,
                 disabledContentColor = blackSoft
-            ),
+            )
         ) {
             Text(
-                text = "Cerrar Sesión",
-                fontFamily = Util.loadFontFamilyFromAssets(),
+                text = if (!isEditing) "Cerrar Sesión" else "Cancelar",
+                fontFamily = aladinFont,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold
             )
         }
     }
-}
-
-@Composable
-fun DetailsText(text: String, color: Color, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
-        fontFamily = Util.loadFontFamilyFromAssets(),
-        fontSize = 32.sp,
-        color = color,
-        modifier = modifier
-    )
 }
--- app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsViewModel.kt
+++ app/src/main/java/com/grl/clientapptfg/ui/screens/user_details/UserDetailsViewModel.kt
@@ -3,15 +3,89 @@ package com.grl.clientapptfg.ui.screens.user_details
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.grl.clientapptfg.core.UserSession
+import com.grl.clientapptfg.data.models.UserModel
+import com.grl.clientapptfg.data.repositories.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserDetailsViewModel @Inject constructor() : ViewModel() {
+class UserDetailsViewModel @Inject constructor(private val userRepository: UserRepository) :
+    ViewModel() {
     private val _closeSession = MutableLiveData<Boolean>()
     val closeSession: LiveData<Boolean> = _closeSession
 
-    fun changeCloseSession(boolean: Boolean){
+    private val _phone = MutableLiveData<String>()
+    val phone: LiveData<String> = _phone
+
+    private val _address = MutableLiveData<String>()
+    val address: LiveData<String> = _address
+
+    private val _isUpdateEnable = MutableLiveData<Boolean>()
+    val isUpdateEnable: LiveData<Boolean> = _isUpdateEnable
+
+    private val _isEditing = MutableLiveData<Boolean>()
+    val isEditing: LiveData<Boolean> = _isEditing
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _wantToUpdate = MutableLiveData<Boolean>()
+    val wantToUpdate: LiveData<Boolean> = _wantToUpdate
+
+    private val _updateWasGood = MutableLiveData<Boolean>()
+    val updateWasGood: LiveData<Boolean> = _updateWasGood
+
+    fun setPhone(newText: String) {
+        _phone.value = newText.trim()
+    }
+
+    fun setAddress(newText: String) {
+        _address.value = newText
+    }
+
+    fun changeIsEditing(boolean: Boolean) {
+        _isEditing.value = boolean
+    }
+
+    fun changeWantToUpdate(boolean: Boolean) {
+        _wantToUpdate.value = boolean
+    }
+
+    fun changeUpdateWasGood(boolean: Boolean) {
+        _updateWasGood.value = boolean
+    }
+
+    fun enableUpdate(address: String, phone: String) {
+        _isUpdateEnable.value =
+            phone.length == 9 && address.length > 5
+    }
+
+    fun changeCloseSession(boolean: Boolean) {
         _closeSession.value = boolean
+    }
+
+    fun updateUser(address: String, phone: String, user: UserModel) {
+        _isLoading.value = true
+        viewModelScope.launch {
+            try {
+                var updatedUser = user
+                updatedUser.phone = phone
+                updatedUser.address = address
+                updatedUser = userRepository.updateUser(updatedUser)
+                if (updatedUser.id != 0) {
+                    UserSession.setUser(updatedUser)
+                    setPhone(updatedUser.phone)
+                    setAddress(updatedUser.address)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                _isLoading.value = false
+                _updateWasGood.value = true
+            }
+        }
     }
 }
